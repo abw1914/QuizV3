6626e8f8ef76a2ca3c1b709168450f2fb418072b
aaron <aaronbwillis@msn.com>
2018-10-17 08:58:30 -0600

    Logic included to check for incorrect responses and allow for no more than 4 responses.

--- app/src/main/java/com/android/quizv2/MainActivity.java
+++ app/src/main/java/com/android/quizv2/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void scoreQuestion1 (View view) {
         RadioButton rb3 = findViewById(R.id.question1_rb3);
 
@@ -32,47 +31,60 @@ public class MainActivity extends AppCompatActivity {
     }
     public void scoreQuestion3(View view) {
         EditText stateResponse = (EditText) findViewById(R.id.question3_response);
-
     }
 
     public void scoreQuestion4(View view) {
-
     }
+
     public int scoreTotal() {
         int totalScore1;
+        RadioButton rb1 = findViewById(R.id.question1_rb1);
+        RadioButton rb2 = findViewById(R.id.question1_rb2);
         RadioButton rb3 = findViewById(R.id.question1_rb3);
+        boolean response1 = rb1.isChecked();
+        boolean response2 = rb2.isChecked();
         boolean response3 = rb3.isChecked();
-        if (response3) {
+        if (response3 && !response1 && !response2 ) {
             correctResponse +=1;
         } else {
             incorrectResponse++;
         }
-        CheckBox cb1 = findViewById(R.id.question2_cb3);
-        boolean response1 = cb1.isChecked();
-        if(response1) {
+        CheckBox cb1 = findViewById(R.id.question2_cb1);
+        CheckBox cb2 = findViewById(R.id.question2_cb2);
+        CheckBox cb3 = findViewById(R.id.question2_cb3);
+        boolean res1 = cb1.isChecked();
+        boolean res2 = cb2.isChecked();
+        boolean res3 = cb3.isChecked();
+        if(res3 && !res1 && !res2) {
             correctResponse +=1;
         } else {
             incorrectResponse++;
         }
         EditText stateResponse = (EditText) findViewById(R.id.question3_response);
         if(stateResponse.getText().toString().equals("Colorado")) {
-            correctResponse+=1;;
-            //displayResponses(correctResponse);
+            correctResponse+=1;
+
         } else {
             incorrectResponse++;
         }
+        CheckBox q4cb2 = findViewById(R.id.question4_cb2);
+        CheckBox q4cb3 = findViewById(R.id.question4_cb3);
         CheckBox q4cb1 = findViewById(R.id.question4_cb1);
         boolean resp1 = q4cb1.isChecked();
-        if(resp1) {
+        boolean resp2 = q4cb2.isChecked();
+        boolean resp3 = q4cb3.isChecked();
+        if(resp1 && !resp2 && !resp3) {
             correctResponse+=1;
         } else {
             incorrectResponse++;
         }
 
-        if(correctResponse > 4){
+        if(correctResponse > 4 || incorrectResponse > 4){
             Toast.makeText(this, "Restart the App to Retake the Quiz", Toast.LENGTH_SHORT).show();
             correctResponse = 0;
+            incorrectResponse = 0;
         }
+
         return correctResponse;
     }
 
